53978420e08d76be214cbe2def474ab6d7ad90d2
snfazal <snfazal@gmail.com>
2017-03-21 10:26:52 -0400

    took out needless toggle functionality, projects no longer attached

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,33 @@
-$( document ).ready(function(){
-  $(".button-collapse").sideNav({
-      menuWidth: 150,
-      edge: 'left',
-      closeOnClick: true,
-      draggable: true
-  });
+// $( document ).ready(function(){
+//   $(".button-collapse").sideNav({
+//       menuWidth: 150,
+//       edge: 'left',
+//       closeOnClick: true,
+//       draggable: true
+//   });
 
-  $('a').smoothScroll({
-    offset: -64,
-    exclude: ['#test-swipe-1', '#test-swipe-2', '#test-swipe-3'],
-    excludeWithin: ['#tabs-swipe-demo']
-  });
+      //let's users scroll through projects 
+//   $('a').smoothScroll({
+//     offset: -64,
+//     exclude: ['#test-swipe-1', '#test-swipe-2', '#test-swipe-3'],
+//     excludeWithin: ['#tabs-swipe-demo']
+//   });
+//
+//   $('ul.tabs').tabs({
+//     swipeable: true
+//   });
+      // makes projects open up and collapsible
+//   $('.collapsible').collapsible();
 
-  $('ul.tabs').tabs({
-    swipeable: true
-  });
+      // changes color of projects
+//   $('.li').hover(function(){
+//     // console.log(this.children[0].children[0]);
+//     $(this.children[0].children[1].children).toggleClass('colored');
+//     $(this.children[0].children[0]).toggleClass('fancy');
+//   });
+//
+// });
 
-  $('.collapsible').collapsible();
+// Toggle functionality lost bc commits with project attached to them have also been lost.
 
-  $('.li').hover(function(){
-    // console.log(this.children[0].children[0]);
-    $(this.children[0].children[1].children).toggleClass('colored');
-    $(this.children[0].children[0]).toggleClass('fancy');
-  });
-
-});
+// Source for toggle functionality: http://api.jquery.com/toggle/, https://esri.github.io/,
